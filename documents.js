e2983ffc9d7d55832ab1ba4efc8dbb86ae31e230
Nikita Kazhin <152276468+nkazhin@users.noreply.github.com>
2025-08-26 17:00:55 +0300

    Create documents.js

---
+++ documents.js
@@ -0,0 +1,72 @@
+/**
+ * Document database with Anthropic File API IDs and corresponding URLs
+ */
+const documents = [
+  {
+    fileId: 'file_011CSWMNdRHEtvdXRJN7pqy9',
+    title: 'Профилактика бешенства',
+    url: 'https://bit.ly/rabies_vaccine'
+  },
+  {
+    fileId: 'file_011CSWMaJrAou1vGrN5hzN4c',
+    title: 'Как подготовиться к прививке и вести себя во время и после вакцинации',
+    url: 'https://bit.ly/brochures-prepare-for-vaccination'
+  },
+  {
+    fileId: 'file_011CSWNuGrWY3zMbmSdqNC6w',
+    title: '7 способов профилактики гриппа',
+    url: 'https://bit.ly/prophylaxis_flu'
+  },
+  {
+    fileId: 'file_011CSWP1oebVjZVWCfrRTy2g',
+    title: 'Вакцинация взрослых от пневмококковой инфекции',
+    url: 'https://bit.ly/booklet_pneumococc_adults'
+  },
+  {
+    fileId: 'file_011CSWP5pUKKAE59SWTv8E7x',
+    title: 'Вакцинация пациентов с первичными иммунодефицитами',
+    url: 'https://bit.ly/brochures-reminders-special-groups'
+  },
+  {
+    fileId: 'file_011CSWP92yyFoAJKJntHT4tP',
+    title: 'Вакцинация недоношенных и маловесных детей',
+    url: 'https://bit.ly/premature_vax'
+  },
+  {
+    fileId: 'file_011CSWNwmAAW6jdGuXK3Dt7V',
+    title: 'ВПЧ и прививка от них',
+    url: 'https://bit.ly/HPV-and-vaccines'
+  },
+  {
+    fileId: 'file_011CSWPC8yy7fTsowV13XneG',
+    title: 'Гепатит B и прививка от него',
+    url: 'https://bit.ly/booklet_hbv'
+  },
+  {
+    fileId: 'file_011CSWPNqypSGtnrJCjQGQpM',
+    title: 'Клещевой энцефалит и прививка от него 2025',
+    url: 'https://bit.ly/tbe_vaccine'
+  },
+  {
+    fileId: 'file_011CSWPceeLTyELYngd9KSEo',
+    title: 'Прививки и беременность',
+    url: 'https://bit.ly/booklet_pregnancy'
+  },
+  {
+    fileId: 'file_011CSWPS6VHA81wVbRzeHH6w',
+    title: 'Прививки и путешествия по России',
+    url: 'https://disk.yandex.ru/d/kJzSu6QlaIwhyg'
+  },
+  {
+    fileId: 'file_011CSWPUnuC7NLnDQL8Q75fU',
+    title: 'Разобщение детей в связи с проведением вакцинации оральной полиовирусной вакциной (ОПВ)',
+    url: 'https://bit.ly/oral_polio'
+  },
+  {
+    fileId: 'file_011CSWPZUkC7qUfCAQCx86NL',
+    title: 'Туберкулез и прививка от него',
+    url: 'https://bit.ly/booklet_tb'
+  }
+];
+
+module.exports = { documents };
