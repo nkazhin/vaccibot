1306de85f9986133d7b57d4ac4c292014484bece
Nikita Kazhin <152276468+nkazhin@users.noreply.github.com>
2025-08-27 11:12:33 +0300

    Update documents.js

--- documents.js
+++ documents.js
@@ -3,67 +3,67 @@
  */
 const documents = [
   {
-    fileId: 'file_011CSWMNdRHEtvdXRJN7pqy9',
+    fileId: 'file_011CSXsfhXKUKxvHEjSAvxNA',
     title: 'Профилактика бешенства',
     url: 'https://bit.ly/rabies_vaccine'
   },
   {
-    fileId: 'file_011CSWMaJrAou1vGrN5hzN4c',
+    fileId: 'file_011CSXsdRTNmkXvWyxNxewAA',
     title: 'Как подготовиться к прививке и вести себя во время и после вакцинации',
     url: 'https://bit.ly/brochures-prepare-for-vaccination'
   },
   {
-    fileId: 'file_011CSWNuGrWY3zMbmSdqNC6w',
+    fileId: 'file_011CSXs8KhYAmKWxnL45mzYE',
     title: '7 способов профилактики гриппа',
     url: 'https://bit.ly/prophylaxis_flu'
   },
   {
-    fileId: 'file_011CSWP1oebVjZVWCfrRTy2g',
+    fileId: 'file_011CSXsM2K8eknpBfjZ5T7Kr',
     title: 'Вакцинация взрослых от пневмококковой инфекции',
     url: 'https://bit.ly/booklet_pneumococc_adults'
   },
   {
-    fileId: 'file_011CSWP5pUKKAE59SWTv8E7x',
+    fileId: 'file_011CSXsS5qr1GGmfrkVxUHw5',
     title: 'Вакцинация пациентов с первичными иммунодефицитами',
     url: 'https://bit.ly/brochures-reminders-special-groups'
   },
   {
-    fileId: 'file_011CSWP92yyFoAJKJntHT4tP',
+    fileId: 'file_011CSXsPwB9khJ8xXqW2ifX3',
     title: 'Вакцинация недоношенных и маловесных детей',
     url: 'https://bit.ly/premature_vax'
   },
   {
-    fileId: 'file_011CSWNwmAAW6jdGuXK3Dt7V',
+    fileId: 'file_011CSXsJLonTRY61w3xyTZH9',
     title: 'ВПЧ и прививка от них',
     url: 'https://bit.ly/HPV-and-vaccines'
   },
   {
-    fileId: 'file_011CSWPC8yy7fTsowV13XneG',
+    fileId: 'file_011CSXsUZQbYanqwMHke7SnR',
     title: 'Гепатит B и прививка от него',
     url: 'https://bit.ly/booklet_hbv'
   },
   {
-    fileId: 'file_011CSWPNqypSGtnrJCjQGQpM',
+    fileId: 'file_011CSXsXKivECQZp9bMR7eZt',
     title: 'Клещевой энцефалит и прививка от него 2025',
     url: 'https://bit.ly/tbe_vaccine'
   },
   {
-    fileId: 'file_011CSWPceeLTyELYngd9KSEo',
+    fileId: 'file_011CSXsDcetzkyNL8oY8SmCD',
     title: 'Прививки и беременность',
     url: 'https://bit.ly/booklet_pregnancy'
   },
   {
-    fileId: 'file_011CSWPS6VHA81wVbRzeHH6w',
+    fileId: 'file_011CSXsi6iPSqfrC5SJjgTFV',
     title: 'Прививки и путешествия по России',
     url: 'https://disk.yandex.ru/d/kJzSu6QlaIwhyg'
   },
   {
-    fileId: 'file_011CSWPUnuC7NLnDQL8Q75fU',
+    fileId: 'file_011CSXsn2F7cVP39rgsPokEp',
     title: 'Разобщение детей в связи с проведением вакцинации оральной полиовирусной вакциной (ОПВ)',
     url: 'https://bit.ly/oral_polio'
   },
   {
-    fileId: 'file_011CSWPZUkC7qUfCAQCx86NL',
+    fileId: 'file_011CSXsp3iyJfjZK9Hp8ZgYg',
     title: 'Туберкулез и прививка от него',
     url: 'https://bit.ly/booklet_tb'
   }
